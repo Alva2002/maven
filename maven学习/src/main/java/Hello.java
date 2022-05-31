479a42a540e8907e76d7438c17d3fdacbc81482d
Alva <9919546+alva12345@user.noreply.gitee.com>
2022-05-31 20:51:50 +0800

    我的第一个maven

---
+++ maven学习/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author alva
+ * @create 2022-05-31 20:43
+ * @desc 第一
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("好好学习");
+    }
+}
+
